a943491415f7e0743897cd3ba06fd5b77e6ec75b
Rishackmoto <rishackmoto@gmail.com>
2025-11-27 11:16:37 +0900

    backend api push

--- index.js
+++ index.js
@@ -57,4 +57,7 @@ app.get("/surat", async (req, res) => {
     return res.json({ success: false, message: err.message });
   }
 });
-app.listen(3000, () => console.log("API running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
